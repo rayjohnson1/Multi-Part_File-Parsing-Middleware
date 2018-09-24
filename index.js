507df5b7e64cd7682b04afa8cf8c1627d6a9a016
rayjohnson1 <39462262+rayjohnson1@users.noreply.github.com>
2018-09-24 08:09:38 -0400

    Encapsulated fileParser into a middleware function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 //Must be used before Router Declaration
 function modifyReqObj(req, res, next){
     req.body = {};
+    req.files = [];
     next();
 }
 
@@ -32,67 +33,80 @@ const router = express.Router();
 app.use('/api', router);
 
 //Router
-router.post('/encoding', function (req, res, next){
-    let data = "";
-    req.on('data', (chunk) => {
-        
-        data += chunk;
-    });
-
-    req.on('end', (err) => {
-
-        let boundary = data.match(/^.+$/m);
-        data = data.split(boundary);
-        console.log(data.length)
-        req.body.files = [];
-        for(var i = 0; i < data.length; i++){
-            
-            let isFile = false;
-            if(data[i].search(/filename=/) !== -1)
-                isFile = true;
-
-            if(isFile){
-
-                let file = data[i].match(/^(?!Content).+$/gm)
-                //console.log(file)
-
-                let mimeType = data[i].match(/^content-type:.+/mi)[0];
-                //console.log(mimeType)
-
-                let fileExtension = mimeType.split('/')[1]
-                //console.log(fileExtension)
-
-                let contentDisposition = data[i].match(/^content-disposition:.+/igm)[0];
-                //console.log(contentDisposition)
-
-                let inputData = contentDisposition.match(/(["']\w.+?['"])/mig)
-                let formInputName = inputData[0].replace(/["]/mig, "");
-                let fileName = inputData[1].replace(/["]/mig, "");;
-                
-                req.body.files.push({
-                    mimeType,
-                    fileExtension,
-                    fileName,
-                    location: `tmp/${fileName}${i}.${fileExtension}`
-                })
-                fs.writeFile(`tmp/${fileName}${i}.${fileExtension}`, file, 'base64', () => {
-                    //console.log(`uploaded file ${i}`)
-                });
-                
-
-            }//else
-                //console.log('This wasnt a file so we skipped')
-
-        }
-        next();
-    });
-
-    
-
-}, function(req, res){
-    console.log(req.body)
+router.post('/encoding', fileParser, function(req, res){
+    console.log(req.files)
 });
 
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+function fileParser(req, res, next){
+
+    //Check that Content-Type for multipart/form-data
+    if(req.get('content-type') === 'multipart/form-data')
+        handleData();
+
+    function isFile(binaryData){
+        return binaryData.search(/filename=/) !== -1;
+    }
+
+    function handleData(){
+        let data = "";
+
+        // Populate our data string with chunk values
+        req.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        req.on('end', (err) => {
+
+            //Caputre form Boundary
+            let boundary = data.match(/^.+$/m);
+
+            //Split up all files 
+            let binaryFiles = data.split(boundary);
+
+            for(var i = 0; i < binaryFiles.length; i++){
+    
+                if(isFile(binaryFiles[i])){
+    
+                    let file = binaryFiles[i].match(/^(?!Content).+$/gm);
+                    //console.log(file)
+    
+                    let mimeType = binaryFiles[i].match(/^content-type:.+/mi)[0];
+                    //console.log(mimeType)
+    
+                    let fileExtension = mimeType.split('/')[1];
+                    //console.log(fileExtension)
+    
+                    let contentDisposition = binaryFiles[i].match(/^content-disposition:.+/igm)[0];
+                    //console.log(contentDisposition)
+    
+                    let inputData = contentDisposition.match(/(["']\w.+?['"])/mig)
+                    let formInputName = inputData[0].replace(/["]/mig, "");
+                    let fileName = inputData[1].replace(/["]/mig, "");
+    
+                    //Removes file extension off of the end of file name
+                    let nameParts = fileName.split(".");
+                    nameParts.pop();
+                    fileName = nameParts.join(".");
+
+                    req.files.push({
+                        mimeType,
+                        fileExtension,
+                        fileName,
+                        location: `tmp/${fileName}.${fileExtension}`
+                    });
+
+                    fs.writeFile(`tmp/${fileName}.${fileExtension}`, file, 'base64');
+                }
+            }
+
+            next();
+        });
+
+    }
+
+}
